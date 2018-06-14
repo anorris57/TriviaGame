c323f50320089ea0ac6ca3aae9d6d18998d734d4
Angela Norris <cocoanorris@yahoo.com>
2018-06-14 06:30:48 -0400

    correcting formatting of code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,7 @@ var number = 60;
 
 var intervalID;
 //Changing line #7
-$("#start").click(function(){
+$("#start").click(function () {
   run();
   $("#hideStart").hide();
   $("form").removeClass("hidden");
@@ -13,7 +13,7 @@ $("#start").click(function(){
 });//end for start
 
 function run() {
-  
+
   intervalID = setInterval(decrement, 1000);
 }
 
@@ -29,48 +29,48 @@ function decrement() {
   }
 }
 
-function stop () {
+function stop() {
   clearInterval(intervalID);
   $("#viewQuestions").hide();
   $("#end").hide();
   submitAnswers();
 }
-  
+
 $("#end").on("click", stop);
 
 function submitAnswers() {
   var total = 8;
   var score = 0;
-  
+
 
   //Get User Input
-  var q1 = document.forms["quizForm"] ["q1"].value;//gives value of user's choice
-  var q2 = document.forms["quizForm"] ["q2"].value;
-  var q3 = document.forms["quizForm"] ["q3"].value;
-  var q4 = document.forms["quizForm"] ["q4"].value;
-  var q5 = document.forms["quizForm"] ["q5"].value;
-  var q6 = document.forms["quizForm"] ["q6"].value;
-  var q7 = document.forms["quizForm"] ["q7"].value;
-  var q8 = document.forms["quizForm"] ["q8"].value;
-    //alert(q1);
-    //Set Correct Answers
-  var answers = ["a","c","d","a","b","b","d","b"];
+  var q1 = document.forms["quizForm"]["q1"].value;//gives value of user's choice
+  var q2 = document.forms["quizForm"]["q2"].value;
+  var q3 = document.forms["quizForm"]["q3"].value;
+  var q4 = document.forms["quizForm"]["q4"].value;
+  var q5 = document.forms["quizForm"]["q5"].value;
+  var q6 = document.forms["quizForm"]["q6"].value;
+  var q7 = document.forms["quizForm"]["q7"].value;
+  var q8 = document.forms["quizForm"]["q8"].value;
+  //alert(q1);
+  //Set Correct Answers
+  var answers = ["a", "c", "d", "a", "b", "b", "d", "b"];
 
   //Check Answers changing order to try and get correct output/don't change
-  for(i = 1; i <= total; i++) {
-    if(eval('q' + i) === answers[i - 1]) {
+  for (i = 1; i <= total; i++) {
+    if (eval('q' + i) === answers[i - 1]) {
       score++;
-    } 
+    }
   }
-  
+
 
 
   //console.log(score);
- $("#results").html('<h3>Done</h3>Correct Answers:  '+ score + '<br><br>Incorrect Answers: '+ (total - score) );
-  
+  $("#results").html('<h3>Done</h3>Correct Answers:  ' + score + '<br><br>Incorrect Answers: ' + (total - score));
+
   return false;
 }
 
 
-  
+
 
